9f8653635da77a1a0041a17f9fb39138758f6025
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-11-01 13:34:28 +0100

    Update PlayerMovement.cs

--- Ominous Eye/Assets/PlayerMovement.cs
+++ Ominous Eye/Assets/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
+    Animator anim;
+
     Rigidbody2D body;
 
     float horizontal;
@@ -8,11 +10,15 @@ public class PlayerMovement : MonoBehaviour
 
     public float runSpeed = 10.0f;
 
+    private bool isEye;
+
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        isEye = false;
     }
 
     // Update is called once per frame
@@ -20,10 +26,27 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if (horizontal < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
     }
 
     private void FixedUpdate()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            isEye = true;
+        }
     }
 }
