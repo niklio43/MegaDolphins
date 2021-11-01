01498427f16e1cd25d14e6246f0856c8cfa4f1be
Pontustorn <35564939+Pontustorn@users.noreply.github.com>
2021-11-01 13:21:58 +0100

    Movement

---
+++ Ominous Eye/Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+}
