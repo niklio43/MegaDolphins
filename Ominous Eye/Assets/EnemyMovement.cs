bc4c69932f488a6fe2c1aa1af748d588ef2a0b33
Niklas Jörgensen <niklio15csgo@hotmail.com>
2021-11-02 10:28:39 +0100

    fixed unique enemy player movement

--- Ominous Eye/Assets/EnemyMovement.cs
+++ Ominous Eye/Assets/EnemyMovement.cs
@@ -10,6 +10,14 @@ public class EnemyMovement : MonoBehaviour
 
     public float runSpeed = 10.0f;
 
+    private bool isJumping;
+    private bool isEnemy;
+
+    private float jumpForce;
+    private float moveSpeed;
+    private float moveHorizontal;
+    private float moveVertical;
+
     private bool lbMovement;
 
     // Start is called before the first frame update
@@ -18,42 +26,62 @@ public class EnemyMovement : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         lbMovement = false;
-
+        isEnemy = false;
+        isJumping = false;
+        moveSpeed = 3f;
+        jumpForce = 40f;
         this.gameObject.GetComponent<EnemyMovement>().enabled = false;
-        Debug.Log("Movement false:" + this.gameObject.name);
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = vertical = Input.GetAxisRaw("Vertical");
 
-        if (horizontal > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-        if (horizontal < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-
-        lbMovement = horizontal != 0;
+        lbMovement = moveHorizontal != 0;
         anim.SetBool("lbMovement", lbMovement);
     }
 
     private void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        if (moveHorizontal > 0f || moveHorizontal < 0f)
+        {
+            body.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+        if (moveHorizontal > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (moveHorizontal < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (!isJumping && isEnemy)
+        {
+            body.AddForce(new Vector2(0f, moveVertical * jumpForce), ForceMode2D.Impulse);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-                this.gameObject.GetComponent<EnemyMovement>().enabled = true;
-                Debug.Log("Movement true: "+this.gameObject.name);
+            isEnemy = true;
+            this.gameObject.GetComponent<EnemyMovement>().enabled = true;
+        }
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isJumping = true;
         }
     }
 }
